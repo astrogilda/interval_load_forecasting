ed48cc4d3b8fbba586c2f051a47dc598bcba0684
Sankalp <sankalp.gilda@gmail.com>
2023-08-18 16:10:19 -0400

    refactor: added rolling stats and option to save

--- time_series_eda.py
+++ time_series_eda.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Union
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -7,10 +7,17 @@ from pandas.plotting import autocorrelation_plot
 from statsmodels.graphics.tsaplots import plot_pacf
 from statsmodels.tsa.seasonal import MSTL
 
+from time_constants import (
+    DAYS_PER_MONTH,
+    DAYS_PER_WEEK,
+    FIFTEEN_MINUTES_PER_HOUR,
+    HOURS_PER_DAY,
+)
+
 # Define the settings for high-quality plots
 plot_settings = {
-    "figure.figsize": (14, 8),  # Figure size
-    "font.size": 12,  # Font size
+    "figure.figsize": (16, 8),  # Figure size
+    "font.size": 15,  # Font size
     "font.family": "serif",  # Font family
     "lines.linewidth": 3,  # Line width
     "axes.labelsize": 16,  # Label size
@@ -33,7 +40,14 @@ plt.rcParams.update(plot_settings)
 
 
 class TimeSeriesEDA:
-    def plot_time_series(self, y: pd.Series) -> None:
+    def plot_time_series(
+        self,
+        y: Union[pd.Series, list[pd.Series]],
+        title: str,
+        label: Union[str, list[str]] = "",
+        color: Union[str, list[str]] = "blue",
+        savefig_name: Optional[str] = None,
+    ) -> None:
         """
         Plots the time series data.
 
@@ -42,17 +56,47 @@ class TimeSeriesEDA:
         y : pd.Series
             Time series data.
         """
-        plt.figure()  # figsize=FIGSIZE)
-        plt.plot(y)
-        plt.title("Time Series Plot")
+        # If y or label or color is list, ensure that they are all lists of the same length
+        if (
+            isinstance(y, list)
+            and isinstance(label, list)
+            and isinstance(color, list)
+        ):
+            if len(y) != len(label) or len(y) != len(color):
+                raise ValueError(
+                    "If y, label, and color are lists, they must all have the same length"
+                )
+        # If y or label or color is not a list, convert it to a list
+        elif (
+            not isinstance(y, list)
+            and not isinstance(label, list)
+            and not isinstance(color, list)
+        ):
+            y = [y]
+            label = [label]
+            color = [color]
+        else:
+            raise TypeError(
+                "If y, label, and color are lists, they must all have the same length"
+            )
+
+        plt.figure()
+        for y_, label_, color_ in zip(y, label, color):
+            plt.plot(y_, label=label_, color=color_)
+        plt.legend(loc="best", fontsize=12)
+        plt.title(title)
         plt.xlabel("Time")
-        plt.ylabel("Value")
-        plt.show()
+        plt.ylabel("Load")
+        if savefig_name is not None:
+            plt.savefig(savefig_name)
+        else:
+            plt.show()
 
     def plot_seasonal_decomposition(
         self,
         y: pd.Series,
         seasonalities: Optional[List[int]] = None,
+        savefig_name: Optional[str] = None,
     ) -> None:
         """
         Plots the seasonal decomposition of the time series data using STL.
@@ -66,16 +110,26 @@ class TimeSeriesEDA:
         """
         if seasonalities is None:
             seasonalities = [
-                96,
-                96 * 7,
-                96 * 30,
-            ]  # 96 15-minute intervals in a day, 96*7 in a week
+                FIFTEEN_MINUTES_PER_HOUR * HOURS_PER_DAY,  # 1 day
+                FIFTEEN_MINUTES_PER_HOUR
+                * HOURS_PER_DAY
+                * DAYS_PER_WEEK,  # 1 week
+                FIFTEEN_MINUTES_PER_HOUR
+                * HOURS_PER_DAY
+                * DAYS_PER_MONTH,  # 1 month
+            ]
         mstl = MSTL(y, periods=seasonalities)
         result = mstl.fit()
         plt.close("all")
         result.plot()
+        if savefig_name is not None:
+            plt.savefig(savefig_name)
+        else:
+            plt.show()
 
-    def plot_acf_pacf(self, y: pd.Series, lags: int) -> None:
+    def plot_acf_pacf(
+        self, y: pd.Series, lags: int, savefig_name: Optional[str] = None
+    ) -> None:
         """
         Plots the Autocorrelation Function (ACF) and Partial Autocorrelation Function (PACF).
 
@@ -87,6 +141,7 @@ class TimeSeriesEDA:
             Number of lags to consider.
         """
         plt.figure()
+
         plt.subplot(211)
         autocorrelation_plot(y)
         plt.title("Autocorrelation Function (ACF)")
@@ -94,9 +149,15 @@ class TimeSeriesEDA:
         plt.subplot(212)
         plot_pacf(y, lags=lags)
         plt.title("Partial Autocorrelation Function (PACF)")
-        plt.show()
 
-    def plot_distribution(self, y: pd.Series) -> None:
+        if savefig_name is not None:
+            plt.savefig(savefig_name)
+        else:
+            plt.show()
+
+    def plot_distribution(
+        self, y: pd.Series, savefig_name: Optional[str] = None
+    ) -> None:
         """
         Plots the distribution of the time series data.
 
@@ -110,7 +171,10 @@ class TimeSeriesEDA:
         plt.title("Distribution Plot")
         plt.xlabel("Value")
         plt.ylabel("Frequency")
-        plt.show()
+        if savefig_name is not None:
+            plt.savefig(savefig_name)
+        else:
+            plt.show()
 
     def plot_patterns(
         self,
@@ -118,6 +182,7 @@ class TimeSeriesEDA:
         target_variable: str,
         x_variable: str,
         title: str,
+        savefig_name: Optional[str] = None,
     ) -> None:
         """
         Plots the patterns of the time series data.
@@ -136,7 +201,12 @@ class TimeSeriesEDA:
         plt.figure()
         sns.boxplot(data=df, x=x_variable, y=target_variable)
         plt.title(title)
-        plt.show()
+        plt.xlabel(x_variable)
+        plt.ylabel(target_variable)
+        if savefig_name is not None:
+            plt.savefig(savefig_name)
+        else:
+            plt.show()
 
     def plot_heatmap(
         self,
@@ -144,6 +214,7 @@ class TimeSeriesEDA:
         target_variable: str,
         groupby_vars: list,
         title: str,
+        savefig_name: Optional[str] = None,
     ) -> None:
         """
         Plots a heatmap of the time series data.
@@ -170,7 +241,12 @@ class TimeSeriesEDA:
             linecolor="black",
         )
         plt.title(title)
-        plt.show()
+        plt.xlabel(groupby_vars[1])
+        plt.ylabel(groupby_vars[0])
+        if savefig_name is not None:
+            plt.savefig(savefig_name)
+        else:
+            plt.show()
 
     def perform_eda(
         self,
@@ -206,18 +282,72 @@ class TimeSeriesEDA:
 
         # Call the plotting methods
         y = df[target_variable]
-        self.plot_time_series(y)
+        self.plot_time_series(
+            y, title="Load Time Series", savefig_name="load_time_series.png"
+        )
+
+        # Calculate basic statistics
+        y_mean = y.rolling(
+            window=FIFTEEN_MINUTES_PER_HOUR * HOURS_PER_DAY
+        ).mean()
+        y_median = y.rolling(
+            window=FIFTEEN_MINUTES_PER_HOUR * HOURS_PER_DAY
+        ).median()
+        y_max = y.rolling(
+            window=FIFTEEN_MINUTES_PER_HOUR * HOURS_PER_DAY
+        ).max()
+        y_min = y.rolling(
+            window=FIFTEEN_MINUTES_PER_HOUR * HOURS_PER_DAY
+        ).min()
+
+        # Plot basic statistics
+        self.plot_time_series(
+            [y_mean, y_median, y_max, y_min],
+            label=[
+                "Rolling Mean (24h)",
+                "Rolling Median (24h)",
+                "Rolling Max (24h)",
+                "Rolling Min (24h)",
+            ],
+            title="Rolling Statistics",
+            savefig_name="rolling_statistics.png",
+        )
+
         # self.plot_seasonal_decomposition(y, freq)
-        self.plot_acf_pacf(y, lags)
-        self.plot_distribution(y)
-        self.plot_patterns(df, target_variable, "hour", "Hourly Load Patterns")
-        self.plot_patterns(df, target_variable, "day", "Daily Load Patterns")
+        self.plot_acf_pacf(y, lags, savefig_name="acf_pacf.png")
+        self.plot_distribution(y, savefig_name="distribution.png")
         self.plot_patterns(
-            df, target_variable, "month", "Monthly Load Patterns"
+            df,
+            target_variable,
+            "hour",
+            "Hourly Load Patterns",
+            savefig_name="hourly_load_patterns.png",
+        )
+        self.plot_patterns(
+            df,
+            target_variable,
+            "day",
+            "Daily Load Patterns",
+            savefig_name="daily_load_patterns.png",
+        )
+        self.plot_patterns(
+            df,
+            target_variable,
+            "month",
+            "Monthly Load Patterns",
+            savefig_name="monthly_load_patterns.png",
         )
         self.plot_heatmap(
-            df, target_variable, ["day", "hour"], "Weekly Load Heatmap"
+            df,
+            target_variable,
+            ["day", "hour"],
+            "Weekly Load Heatmap",
+            savefig_name="weekly_load_heatmap.png",
         )
         self.plot_heatmap(
-            df, target_variable, ["month", "hour"], "Annual Load Heatmap"
+            df,
+            target_variable,
+            ["month", "hour"],
+            "Annual Load Heatmap",
+            savefig_name="annual_load_heatmap.png",
         )
